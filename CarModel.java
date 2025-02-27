e720247dd4dbed827d81a1f8efd93ce955af8c0f
TommyProtheroe <C3050950@newcasle.ac.uk>
2025-02-27 23:55:19 +0000

    Creat constructor to initialize CarModel fields.

--- CarModel.java
+++ CarModel.java
@@ -1,7 +1,17 @@
 public class CarModel {
+
     private String name;
     private double weight;
     private double sales_price;
     private int number_sold;
     private String type;
+
+    public CarModel(String name, double weight, double sales_price, int number_sold, String type) {
+        this.name = name;
+        this.weight = weight;
+        this.sales_price = sales_price;
+        this.number_sold = number_sold;
+        this.type = type;
+    }
 }
+
