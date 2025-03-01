586767f50123de412b940ecb27e6c511634249b4
TommyProtheroe <C3050950@newcasle.ac.uk>
2025-03-01 11:53:04 +0000

    Create test case for listing manufacturers

--- Testing.java
+++ Testing.java
@@ -13,5 +13,12 @@ public class Testing {
         CarModel corolla = new CarModel("Corolla", 1300, 22000, 500, "saloon");
         toyota.getCarModels().add(corolla);
         System.out.println("Test 2 Passed: Car Model added - " + corolla.getName());
+
+        System.out.println("\n Test 3 Passed: Listing Manufacturers");
+        for (Manufacturer m : reporting.getManufacturers()) {
+            System.out.println("- " + m.getMake());
+        }
+
+
     }
 }
