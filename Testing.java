22fce502607a20cfad5fdfccfe53d5aef95e7e5a
TommyProtheroe <C3050950@newcasle.ac.uk>
2025-03-01 11:49:54 +0000

    Create test case to add manufacturer

--- Testing.java
+++ Testing.java
@@ -1,5 +1,13 @@
+import java.util.List;
+
 public class Testing {
 
     public static void main(String[] args) {
         Reporting reporting = new Reporting();
+
+
+        Manufacturer toyota = new Manufacturer("Toyota");
+        reporting.getManufacturers().add(toyota);
+        System.out.println("Test 1 Passed: Manufacturer added - " + toyota.getMake());
+    }
 }
