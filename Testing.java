f7799706efa611549a2af988f1b96bc0a146e512
TommyProtheroe <C3050950@newcasle.ac.uk>
2025-03-01 11:51:07 +0000

    Create test case to add car model

--- Testing.java
+++ Testing.java
@@ -9,5 +9,9 @@ public class Testing {
         Manufacturer toyota = new Manufacturer("Toyota");
         reporting.getManufacturers().add(toyota);
         System.out.println("Test 1 Passed: Manufacturer added - " + toyota.getMake());
+
+        CarModel corolla = new CarModel("Corolla", 1300, 22000, 500, "saloon");
+        toyota.getCarModels().add(corolla);
+        System.out.println("Test 2 Passed: Car Model added - " + corolla.getName());
     }
 }
