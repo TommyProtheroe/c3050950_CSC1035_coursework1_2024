a7c68ff66db1c3ecf6f13e5f7690b79ce9fb7aa1
TommyProtheroe <C3050950@newcasle.ac.uk>
2025-03-01 10:45:59 +0000

    Add main method and initialize ReportingIO instance

--- ReportingIO.java
+++ ReportingIO.java
@@ -3,4 +3,11 @@ import java.util.Scanner;
 public class ReportingIO {
     private Reporting reporting = new Reporting();
     private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ReportingIO reportingIO = new ReportingIO();
+    }
+
 }
+
+
