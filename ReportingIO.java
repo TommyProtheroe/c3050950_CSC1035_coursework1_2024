ee100d341a740ddc28b6457f22368b693dcaaa4b
TommyProtheroe <C3050950@newcasle.ac.uk>
2025-03-01 10:46:44 +0000

    Implement runMenu method with a loop and user input handling

--- ReportingIO.java
+++ ReportingIO.java
@@ -6,8 +6,27 @@ public class ReportingIO {
 
     public static void main(String[] args) {
         ReportingIO reportingIO = new ReportingIO();
+        reportingIO.runMenu();
+    }
+
+    public void runMenu() {
+        while (true) {
+            System.out.println("\nCar Investor Reporting System");
+            System.out.println("1. Enter Manufacturer Data");
+            System.out.println("2. Enter Car Model Data");
+            System.out.println("3. List All Manufacturers");
+            System.out.println("4. List Car Models by Manufacturer");
+            System.out.println("5. Generate Reports");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+        }
     }
 
 }
+
+
 
 
